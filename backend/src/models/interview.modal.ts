9e272dea4cee434e21f101331ae97617f5017b8e
lijo ns <lijons13@gmail.com>
2025-05-18 21:37:06 +0530

    interview modal updated

--- backend/src/models/interview.modal.ts
+++ backend/src/models/interview.modal.ts
@@ -3,8 +3,10 @@ import mongoose, { Document, Schema } from "mongoose";
 export interface IInterview extends Document {
   companyId: mongoose.Types.ObjectId;
   userId: mongoose.Types.ObjectId;
+  applicationId: mongoose.Types.ObjectId;
   scheduledAt: Date;
   status: "pending" | "accepted" | "rejected";
+  result: "pending" | "pass" | "fail";
   roomId: string;
 }
 
@@ -12,10 +14,20 @@ const InterviewSchema = new Schema<IInterview>(
   {
     companyId: { type: Schema.Types.ObjectId, ref: "User", required: true },
     userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    applicationId: {
+      type: Schema.Types.ObjectId,
+      ref: "Application",
+      required: true,
+    },
     scheduledAt: { type: Date, required: true },
     status: {
       type: String,
       enum: ["pending", "accepted", "rejected"],
+      default: "pending",
+    },
+    result: {
+      type: String,
+      enum: ["pending", "pass", "fail"],
       default: "pending",
     },
     roomId: { type: String, required: true },
