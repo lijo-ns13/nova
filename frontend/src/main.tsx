dfc665ad16753358767f2d18346225404906f3dd
lijo ns <lijons13@gmail.com>
2025-06-19 12:53:05 +0530

    frontend:helmet added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,20 +6,20 @@ import { store, persistor } from "./store/store";
 import { PersistGate } from "redux-persist/integration/react";
 import "./index.css";
 import ToastProvider from "./features/user/componets/ui/ToastProvider";
+import { HelmetProvider } from "react-helmet-async"; // ✅ Import HelmetProvider
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <ToastProvider>
-          {" "}
-          {/* ✅ Wrap here */}
-          <App />
-        </ToastProvider>
+        <HelmetProvider>
+          <ToastProvider>
+            {" "}
+            {/* ✅ Wrap here */}
+            <App />
+          </ToastProvider>
+        </HelmetProvider>
       </PersistGate>
     </Provider>
   </React.StrictMode>
 );
-// if (!socket.connected) {
-//       socket.connect();
-//       socket.emit("login", userId);
-//     }
