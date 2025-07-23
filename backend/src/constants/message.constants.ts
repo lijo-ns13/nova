7c7d99a2319aadd4f23a79b5b27eec6c15b87a20
lijo ns <lijons13@gmail.com>
2025-07-23 14:28:06 +0530

    updated:message constants

--- backend/src/constants/message.constants.ts
+++ backend/src/constants/message.constants.ts
@@ -1,13 +1,10 @@
 export const MESSAGES = {
-  SUCCESS: {
-    USER_CREATED: "User created successfully.",
-    JOB_UPDATED: "Job updated successfully.",
-    DELETED: "Deleted successfully.",
-  },
-  ERROR: {
-    USER_NOT_FOUND: "User not found.",
-    INVALID_CREDENTIALS: "Invalid credentials.",
-    UNAUTHORIZED: "Unauthorized access.",
-    SERVER_ERROR: "Something went wrong. Please try again later.",
+  USER_INTERVIEW: {
+    SUCCESS: {
+      UPDATED: "Interview status updated",
+      RESCHEDULED: "Interview reschedule status updated",
+      GET_RESCHEDULED_SLOTS: "Reschedule slots retrieved",
+    },
+    ERROR: {},
   },
 };
