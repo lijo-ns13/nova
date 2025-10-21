2b28c001520928784bd73b930e7105d111f33303
lijo ns <lijons13@gmail.com>
2025-10-21 10:24:29 +0530

    fix:add mapper to company service

--- backend/src/services/company/CompanyService.ts
+++ backend/src/services/company/CompanyService.ts
@@ -1,14 +1,22 @@
-import { inject } from "inversify";
+import { inject, injectable } from "inversify";
 import { TYPES } from "../../di/types";
 import { ICompanyRepository } from "../../interfaces/repositories/ICompanyRepository";
 import { ICompanyService } from "../../interfaces/services/ICompanyService";
-import { ICompany } from "../../repositories/entities/company.entity";
+import { CompanyProfileDTO } from "../../core/dtos/company/company.profile.dto";
+import { CompanyProfileMapper } from "../../mapping/company/company.profile.mapper";
+
+@injectable()
 export class CompanyService implements ICompanyService {
   constructor(
     @inject(TYPES.CompanyRepository)
     private readonly _companyRepo: ICompanyRepository
   ) {}
-  async getCompanyData(companyId: string): Promise<ICompany | null> {
-    return await this._companyRepo.findById(companyId);
+
+  async getCompanyData(companyId: string): Promise<CompanyProfileDTO | null> {
+    const company = await this._companyRepo.findById(companyId);
+    if (!company) return null;
+
+    // map to DTO
+    return CompanyProfileMapper.toDTO(company);
   }
 }
